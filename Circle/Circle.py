863559f547eaad3e81d7e54b3fc18525852a88a9
3c01i <69606738+3c01i@users.noreply.github.com>
2021-01-03 11:05:23 -0500

    Add files via upload

---
+++ Circle/Circle.py
@@ -0,0 +1,12 @@
+import math
+def circle_len_r(r, a=360):
+    circle_len = (( 2 * math.pi * r * a ) / 360 )
+    return(circle_len)
+def circle_area_r(r, a=360):
+    circle_area = (( math.pi * (r**2) * a ) / 360 )
+    return(circle_area)
+def circle_r_l(l):
+    r = ( l ) / ( 2 * math.pi )
+    return(r)
+def circle_area_l(l, a=360):
+    return(circle_area_r(circle_r_l(l)))
---
+++ Exponent/Exp.py
@@ -0,0 +1,12 @@
+def fac(i,comp):
+    for k in range(1,i+1):
+        comp *= k
+    return 1/comp
+exp = 1
+comp = 1
+n = int(input())
+n_l = range(1,n+1)
+for i in n_l:
+    comp = 1
+    exp += fac(i,comp)
+print( "exp =" , exp )
